47036bdd21444d2ce359c9c92ad3b781e0f03539
yujiwei <774346810@qq.com>
2019-01-17 17:35:25 +0800

    初始化提交

---
+++ user-service-provider/src/main/java/com/atguigu/gmall/service/MainApplication.java
@@ -0,0 +1,16 @@
+package com.atguigu.gmall.service;
+
+import java.io.IOException;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApplication {
+	
+	public static void main(String[] args) throws IOException {
+		ClassPathXmlApplicationContext ioc = new ClassPathXmlApplicationContext("provider.xml");
+		ioc.start();
+		
+		System.in.read();
+	}
+
+}
