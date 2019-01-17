4c99b8914b74ed49c9e48f91fc97bc0617bff585
yujiwei <774346810@qq.com>
2019-01-17 19:12:27 +0800

    服务提供和服务发现

--- order-service-consumer/src/main/java/com/atguigu/gmall/service/impl/OrderServiceImpl.java
+++ order-service-consumer/src/main/java/com/atguigu/gmall/service/impl/OrderServiceImpl.java
@@ -13,7 +13,7 @@ import com.atguigu.gmall.service.UserService;
  * 1、将服务提供者注册到注册中心（暴露服务）
  * 		1）、导入dubbo依赖（2.6.2）\操作zookeeper的客户端(curator)
  * 		2）、配置服务提供者
- * 
+ *
  * 2、让服务消费者去注册中心订阅服务提供者的服务地址
  * @author lfy
  *
--- user-service-provider/src/main/java/com/atguigu/gmall/service/MainApplication.java
+++ user-service-provider/src/main/java/com/atguigu/gmall/service/MainApplication.java
@@ -5,11 +5,11 @@ import java.io.IOException;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MainApplication {
-	
+
 	public static void main(String[] args) throws IOException {
 		ClassPathXmlApplicationContext ioc = new ClassPathXmlApplicationContext("provider.xml");
 		ioc.start();
-		
+
 		System.in.read();
 	}
 
